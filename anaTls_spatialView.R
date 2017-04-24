7df0968ea9879dd36e4018a568f01747edd32a21
honghlaries@126.com <honghlaries@126.com>
2017-04-24 14:16:50 +0800

    gether_plot_krig_element

--- anaTls_spatialView.R
+++ anaTls_spatialView.R
@@ -1,44 +1,79 @@
 source("uniTls_pkgInstall.R");source("uniTls_presetPaths.R")
 
-spView.grid <- function(dat,dir,file,col.gardient,
-                        lonC,latC,lonRange,latRange,zoom,
-                        maptype,mapsource) {
-  pkgLoad("dplyr");pkgLoad("tidyr");pkgLoad("ggplot2");pkgLoad("ggmap");
-  plot <- ggmap(get_map(location = c(lat = latC,lon = lonC), zoom = zoom, 
-                        maptype = maptype, source = mapsource)) + 
+spView.grid <- function(dat,dir,file,leg.name, grad.value, grad.tag,
+                        lonRange,latRange) {
+  pkgLoad("dplyr");pkgLoad("tidyr");pkgLoad("ggplot2");pkgLoad("maptools");
+  pkgLoad("rgdal");pkgLoad("directlabels")
+  #CRS("+proj=tmerc +lat_0=0 +lon_0=108 +k=1 +x_0=36500000 +y_0=0 +ellps=krass +towgs84=15")
+  #CRS("+proj=longlat +ellps=WGS84")
+  #bkmap <- readOGR("data/zone_province.shp")
+  #proj4string(bkmap) <- CRS("+proj=tmerc +lat_0=0 +lon_0=108 +k=1 +x_0=36500000 +y_0=0 +ellps=krass +towgs84=15")
+  #bkmap <- spTransform(bkmap,  CRS("+proj=longlat +ellps=WGS84"))
+  #rivers <- readShapeLines("data/item_rivers.shp")
+  #railway <- readShapeLines("data/item_railway.shp")
+  #roads <- readShapeLines("data/item_roads.shp")
+  bkmap <- readShapePoly("data/bou2_4p.shp")
+  plot <- ggplot() + 
     geom_tile(aes(x = lon, y = lat, fill = value), data = dat) +
-    scale_fill_gradient(low = "blue", high = "orange") +
-    coord_quickmap(xlim = lonRange, ylim = latRange) + 
-    facet_wrap(~trait)
-  ggsave(filename = file, plot = plot)
+    geom_contour(aes(x = lon, y = lat, z = value), col = "black", binwidth = 0.25, data = dat) +
+    geom_polygon(aes(x = long, y = lat, group = group), 
+                 colour = "black", fill = "orange", data = fortify(bkmap)) +
+    scale_fill_gradientn(leg.name, breaks = grad.value, labels = grad.tag,colours = rainbow(15)) +
+    
+    coord_quickmap(xlim = lonRange, ylim = latRange) +
+    facet_wrap(~trait,ncol = 3) + 
+    theme_bw() + 
+    theme(aspect.ratio = 1/2,
+          panel.grid = element_blank(),
+          strip.background = element_blank()
+          )
+  
+  #plot <- direct.label(plot, method="top.pieces")
+  ggsave(filename = paste(dirPreset(dir),"/", file, sep = ""), plot = plot)
   plot
 }
 
 spView <- function(dat,dir,file,col.gardient,
-                   lonC,latC,lonRange,latRange,zoom,
-                   maptype,mapsource) {
-  pkgLoad("dplyr");pkgLoad("tidyr");pkgLoad("ggplot2");pkgLoad("ggmap");pkgLoad("maptools")
+                   lonRange,latRange) {
+  pkgLoad("dplyr");pkgLoad("tidyr");pkgLoad("ggplot2");pkgLoad("maptools");
+  pkgLoad("rgdal");pkgLoad("directlabels")
+  #CRS("+proj=tmerc +lat_0=0 +lon_0=108 +k=1 +x_0=36500000 +y_0=0 +ellps=krass +towgs84=15")
+  #CRS("+proj=longlat +ellps=WGS84")
+  #bkmap <- readOGR("data/zone_province.shp")
+  #proj4string(bkmap) <- CRS("+proj=tmerc +lat_0=0 +lon_0=108 +k=1 +x_0=36500000 +y_0=0 +ellps=krass +towgs84=15")
+  #bkmap <- spTransform(bkmap,  CRS("+proj=longlat +ellps=WGS84"))
+  #rivers <- readShapeLines("data/item_rivers.shp")
+  #railway <- readShapeLines("data/item_railway.shp")
+  #roads <- readShapeLines("data/item_roads.shp")
   bkmap <- readShapePoly("data/bou2_4p.shp")
-  
   plot <- ggplot() + 
     geom_tile(aes(x = lon, y = lat, fill = value), data = dat) +
-    geom_polygon(aes(x = long, y = lat, group = id), 
+    geom_contour(aes(x = lon, y = lat, z = value, colour = ..level..), binwidth = 0.2, data = dat) +
+    geom_polygon(aes(x = long, y = lat, group = group), 
                  colour = "black", fill = "orange", data = fortify(bkmap)) +
-    scale_fill_gradient(low = "blue", high = "red") +
-    coord_quickmap(xlim = lonRange, ylim = latRange) 
-  ggsave(filename = file, plot = plot)
+    scale_fill_continuous(low = "blue", high = "red") +
+    scale_colour_gradient(low = "black", high = "black") +
+    coord_quickmap(xlim = lonRange, ylim = latRange) +
+    theme_bw()
+  
+  plot <- direct.label(plot, method="top.pieces")
+  ggsave(filename = paste(dirPreset(dir),"/", file, sep = ""), plot = plot)
   plot
 }
 
 conveySp <- function(x) {
-  tmp <- strsplit(as.character(x),"°")[[1]]
-  as.numeric(tmp[1]) + as.numeric(substr(tmp[2],1,nchar(tmp[2])-1))/60
+  tmp <- strsplit(as.character(x),"°")
+  out <- NULL
+  for(i in 1:length(tmp)) {
+    out <- c(out,as.numeric(tmp[[i]][1]) + as.numeric(substr(tmp[[i]][2],1,nchar(tmp[[i]][2])-1))/60)
+  }
+  out
 }
 
 doKrig <- function(dat, dat.grid, tag, cutoff, 
                    krigFormula = as.formula(paste(tag,"~1",sep="")), 
-                   modsel, outTpe = "draft") {
-  pkgLoad("sp");pkgLoad("gstat");pkgLoad("gridExtra");pkgLoad("geoR")
+                   modsel, dir) {
+  pkgLoad("sp");pkgLoad("gstat");pkgLoad("gridExtra");pkgLoad("RColorBrewer")
   
   p1 <- spplot(dat, tag, do.log = F, main = tag, xlab = "Longi", ylab = "Lati") 
   mod <- variogram(krigFormula,  dat, cutoff = cutoff)
@@ -48,11 +83,9 @@ doKrig <- function(dat, dat.grid, tag, cutoff,
   col <- brewer.pal(9,"OrRd")
   p3 <- spplot(krig["var1.pred"], main = tag, xlab = "Longi", ylab = "Lati", 
                col.regions = col)
-  
-  if(outTpe == "data") return(krig)
-  if(outTpe == "draft") return(grid.arrange(p1,p3,p2, ncol = 2, widths = c(15,15), heights = c(5,5)))
-  if(outTpe == "hemivar") return(p2)
-  if(outTpe == "map") return(p3)
+  ggsave(filename = paste(dirPreset(dir),"/", "modelfit_",tag,".png", sep = ""), 
+         plot = grid.arrange(p1,p3,p2, ncol = 2, widths = c(15,15), heights = c(5,5)))
+  krig
 }
 
 doKrig.grid <- function(tag) {
--- elemKrig.R
+++ elemKrig.R
@@ -9,7 +9,7 @@ datareadln <- function() { ## data readln
   read.csv("data/result_element.csv") %>%
     dplyr::inner_join(read.csv("data/result_grainSize.csv"), by = c("sampleID" = "sampleID")) %>%
     dplyr::right_join(read.csv("data/meta_splList.csv"), by = c("sampleID" = "sampleID")) %>%
-    dplyr::inner_join(read.csv("data/meta_sites.csv"), by = c("siteID" = "siteID")) %>%
+    dplyr::inner_join(read.csv("data/meta_sites.csv"), by = c("siteID" = "siteID")) %>% 
     dplyr::mutate(orgC = C.ac, AVS = S - S.ac, 
                   isComplete = complete.cases(Al,Fe,Mn,Pb,Cr,Ni,Cu,Zn,As,Cd,C,N,S,orgC,AVS,clay,silt,sand)) %>%
     dplyr::select(siteID:depth,isComplete,Al,Fe,Mn,Pb,Cr,Ni,Cu,Zn,As,Cd,C,N,S,orgC,AVS,clay,silt,sand)
@@ -33,103 +33,103 @@ latiRange <-  seq(from = 33.7, to = 34.9, length.out = 125)
 dat.grid <- data.frame(lat = c(1), lon = c(1))
 for (i in 1:125) {
   for (j in 1:125) {
-    if((longiRange[j] - 119.9) * (latiRange[i] - 33.7) - (longiRange[j] - 120.6) * (latiRange[i] - 34.5) > 0) {
+    #if((longiRange[j] - 119.9) * (latiRange[i] - 33.7) - (longiRange[j] - 120.6) * (latiRange[i] - 34.5) > 0) {
       dat.grid <- rbind(dat.grid, c(latiRange[i],longiRange[j]))
-    }
+    #}
   }
 }
 dat.grid <- dat.grid[-1,]
 coordinates(dat.grid) <- ~lon+lat
 
-dat.grid.silt <- doKrig(dat, dat.grid, log(silt) ~ 1, tag = "silt", modsel = vgm(800,"Sph",1.0), outTpe = "data")
-dat.grid.silt <- as.data.frame(dat.grid.silt) %>% select(lon, lat, silt = var1.pred)
-coordinates(dat.grid.silt) <- ~lon+lat
-
 grid.value.tot <- NULL
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Al) ~ 1, tag = "Al", 
-                                   outTpe = "data",modsel = vgm(0.15,"Sph",0.5))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Al)~1, tag = "Al", cutoff = 1.5, 
+                                   modsel = vgm(0.15,"Sph",0.5), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Al")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Fe) ~ 1, tag = "Fe", 
-                                   outTpe = "data",modsel = vgm(0.01,"Mat",0.5,0.01,kappa = 1))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Fe) ~ 1, tag = "Fe", cutoff = 1.5,
+                                   modsel = vgm(0.01,"Mat",0.5,0.01,kappa = 1), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Fe")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Mn) ~ 1, tag = "Mn", 
-                                   outTpe = "data",modsel = vgm(0.035,"Sph",0.3))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Mn) ~ 1, tag = "Mn", cutoff = 1.5,
+                                   modsel = vgm(0.035,"Sph",0.3), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Mn")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Pb) ~ 1, tag = "Pb", 
-                                   outTpe = "data",modsel = vgm(0.35,"Sph",0.5))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Pb) ~ 1, tag = "Pb", cutoff = 1.5,
+                                   modsel = vgm(0.35,"Sph",0.5), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Pb")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Cr) ~ 1, tag = "Cr", 
-                                   outTpe = "data",modsel = vgm(0.06,"Mat",0.7,kappa = 1))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Cr) ~ 1, tag = "Cr", cutoff = 1.5,
+                                   modsel = vgm(0.06,"Mat",0.7,kappa = 1), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Cr")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Ni) ~ 1, tag = "Ni", 
-                                   outTpe = "data",modsel = vgm(0.06,"Sph",1))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Ni) ~ 1, tag = "Ni", cutoff = 1.5,
+                                   modsel = vgm(0.06,"Sph",1), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Ni")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Cu) ~ 1, tag = "Cu", 
-                                   outTpe = "data",modsel = vgm(0.25,"Sph",0.75))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Cu) ~ 1, tag = "Cu", cutoff = 1.5,
+                                   modsel = vgm(0.25,"Sph",0.75), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Cu")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Zn) ~ 1, tag = "Zn", 
-                                   outTpe = "data",modsel = vgm(0.1,"Sph",0.5))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Zn) ~ 1, tag = "Zn", cutoff = 1.5,
+                                   modsel = vgm(0.1,"Sph",0.5), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Zn")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(As) ~ 1, tag = "As", 
-                                   outTpe = "data",modsel = vgm(0.3,"Sph",0.75))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(As) ~ 1, tag = "As", cutoff = 1.5,
+                                   modsel = vgm(0.3,"Sph",0.75), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "As")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(Cd) ~ 1, tag = "Cd", 
-                                   outTpe = "data",modsel = vgm(0.2,"Sph",0.7))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Cd) ~ 1, tag = "Cd", cutoff = 1.5,
+                                   modsel = vgm(0.2,"Sph",0.7), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Cd")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(C) ~ 1, tag = "C", 
-                                   outTpe = "data",modsel = vgm(0.15,"Sph",0.7))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(C) ~ 1, tag = "C", cutoff = 1.5,
+                                   modsel = vgm(0.15,"Sph",0.7), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "C")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(orgC) ~ 1, tag = "orgC", 
-                                   outTpe = "data",modsel = vgm(0.06,"Mat",0.7,kappa = 1))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(orgC) ~ 1, tag = "orgC", cutoff = 1.5,
+                                   modsel = vgm(0.06,"Mat",0.7,kappa = 1), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "orgC")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(N) ~ 1, tag = "N", 
-                                   outTpe = "data",modsel = vgm(0.25,"Sph",1))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(N) ~ 1, tag = "N", cutoff = 1.5,
+                                   modsel = vgm(0.25,"Sph",1), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "N")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(S) ~ 1, tag = "S", 
-                                   outTpe = "data",modsel = vgm(0.4,"Sph",1))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(S) ~ 1, tag = "S", cutoff = 1.5,
+                                   modsel = vgm(0.4,"Sph",1), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "S")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(AVS) ~ 1, tag = "AVS", 
-                                   outTpe = "data",modsel = vgm(0.3,"Sph",0.7))) %>% 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(S) ~ 1, tag = "AVS", cutoff = 1.5,
+                                   modsel = vgm(0.4,"Sph",1), dir = "element/krig")) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "AVS")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, log(silt) ~ 1, tag = "silt", 
-                                   outTpe = "data",modsel = vgm(200,"Sph",1.0))) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "silt")))
+spView.grid(dat = grid.value.tot %>% 
+              filter(trait == "Cu"|trait == "Zn"|trait == "Pb"|trait == "Cr"|
+                       trait == "Ni"|trait == "As"|trait == "Cd"),
+            leg.name = "Content(mg/kg)",grad.value = log(c(0.075,0.5,1,5,25,50,100)), 
+            grad.tag = c(0.075,0.5,1,5,25,50,100), dir = "element/krig",file = "krig_heavymetals.png",
+            lonRange = c(119.2,121.8),latRange = c(33.7,35)) -> plot.hm
 
-spView.grid(dat = grid.value.tot,file = "tmp.png",col.gardient,
-            lonC = 120.8, latC = 34.2, lonRange = c(119.9,121.8),latRange = c(33.7,34.9), zoom = 9,
-            maptype = "hybrid", mapsource = "google")
+spView.grid(dat = grid.value.tot %>% 
+              filter(trait == "Al"|trait == "Fe"|trait == "Mn"|
+                       trait == "orgC"|trait == "AVS"),
+            leg.name = "Content(mg/kg)",grad.value = log(c(250,500,1000,2500,5000,10000,20000)), 
+            grad.tag = c(250,500,1000,2500,5000,10000,20000), dir = "element/krig",file = "krig_haimeixianghao.png",
+            lonRange = c(119.2,121.8),latRange = c(33.7,35)) -> plot.bkelement
 
-
-doKrig(dat, dat.grid, tag = "Al", cutoff = 1.5, krigFormula = log(Al) ~ 1,
-       outTpe = "draft")
+ggsave(filename = "element/krig/gather_krig_element.png",  
+       plot = grid.arrange(plot.bkelement,plot.hm, ncol = 1, widths = c(15), heights = c(10,15)))
