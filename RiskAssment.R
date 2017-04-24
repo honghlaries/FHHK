3ce332fea8b8bd011c0c6de86ae5ab4f2eae8c8b
honghlaries@126.com <honghlaries@126.com>
2017-04-24 16:02:22 +0800

    gatherplot_krig_igeo+ef

--- RiskAssment.R
+++ RiskAssment.R
@@ -108,7 +108,7 @@ spView.grid(dat = grid.value.tot %>%
                        trait == "Ni"|trait == "As"|trait == "Cd"),
             leg.name = "Geo-accumulation Index",grad.value = c(-2,-1,0,1), 
             grad.tag = c(-2,-1,0,1), dir = "riskAssment/krig",file = "krig_igeo_sel.png",
-            lonRange = c(119.2,121.8),latRange = c(33.7,35))
+            lonRange = c(119.2,121.8),latRange = c(33.7,35))  -> plot.igeo
 
 # for enrichment factor
 dat <- datareadln() %>% 
@@ -123,20 +123,47 @@ dat <- datareadln() %>%
   summarise(value = mean(value)) %>%
   spread(trait, value) %>%
   dplyr::inner_join(read.csv("data/meta_sites.csv"), by = c("siteID" = "siteID")) %>%
-  dplyr::select(siteID:depth,Al,Fe,Mn,Pb,Cr,Ni,Cu,Zn,As,Cd) %>%
-  dplyr::mutate(latitudes = conveySp(latitudes),
-                longitude = conveySp(longitude))
+  dplyr::select(siteID:depth,Al,Fe,Mn,Pb,Cr,Ni,Cu,Zn,As,Cd) 
 dat <- as.data.frame(dat)
-coordinates(dat) <- ~longitude+latitudes
+coordinates(dat) <- ~lon+lat
 
-doKrig(dat, dat.grid, tag = "Al", suffix = "_Ef", modsel = vgm(0.2,"Sph",0.7))
-doKrig(dat, dat.grid, tag = "Fe", suffix = "_Ef", modsel = vgm(0.04,"Lin",0,0.003))
-doKrig(dat, dat.grid, tag = "Mn", suffix = "_Ef", modsel = vgm(0.03,"Sph",0.7))
-doKrig(dat, dat.grid, tag = "Pb", suffix = "_Ef", modsel = vgm(0.025,"Lin",0,0.015))
-doKrig(dat, dat.grid, tag = "Cr", suffix = "_Ef", modsel = vgm(0.03,"Lin",0))
-doKrig(dat, dat.grid, tag = "Ni", suffix = "_Ef", modsel = vgm(0.05,"Lin",0))
-doKrig(dat, dat.grid, tag = "Cu", suffix = "_Ef", modsel = vgm(0.2,"Lin",0))
-doKrig(dat, dat.grid, tag = "Zn", suffix = "_Ef", modsel = vgm(0.4,"Lin",0))
-doKrig(dat, dat.grid, tag = "As", suffix = "_Ef", modsel = vgm(0.4,"Lin",0))
-doKrig(dat, dat.grid, tag = "Cd", suffix = "_Ef", modsel = vgm(0.3,"Lin",0))
+grid.value.tot <- NULL
+grid.value <- as.data.frame(doKrig(dat, dat.grid, tag = "Pb", cutoff = 1.5,
+                                   modsel = vgm(0.1,"Sph",0.75), dir = "riskAssment/krig/ef")) %>% 
+  select(lon, lat, value = var1.pred)
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Pb")))
 
+grid.value <- as.data.frame(doKrig(dat, dat.grid, tag = "Ni", cutoff = 1.5,
+                                   modsel = vgm(0.06,"Sph",1), dir = "riskAssment/krig/ef")) %>% 
+  select(lon, lat, value = var1.pred)
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Ni")))
+
+grid.value <- as.data.frame(doKrig(dat, dat.grid, tag = "Cu", cutoff = 1.5,
+                                   modsel = vgm(0.25,"Sph",0.75), dir = "riskAssment/krig/ef")) %>% 
+  select(lon, lat, value = var1.pred)
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Cu")))
+
+grid.value <- as.data.frame(doKrig(dat, dat.grid, tag = "Zn", cutoff = 1.5,
+                                   modsel = vgm(0.2,"Sph",0.5), dir = "riskAssment/krig/ef")) %>% 
+  select(lon, lat, value = var1.pred)
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Zn")))
+
+grid.value <- as.data.frame(doKrig(dat, dat.grid, tag = "As", cutoff = 1.5,
+                                   modsel = vgm(0.6,"Sph",0.5), dir = "riskAssment/krig/ef")) %>% 
+  select(lon, lat, value = var1.pred)
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "As")))
+
+grid.value <- as.data.frame(doKrig(dat, dat.grid, tag = "Cd", cutoff = 1.5,
+                                   modsel = vgm(0.15,"Sph",0.5), dir = "riskAssment/krig/ef")) %>% 
+  select(lon, lat, value = var1.pred)
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Cd")))
+
+spView.grid(dat = grid.value.tot %>% 
+              filter(trait == "Cu"|trait == "Zn"|trait == "Pb"|
+                       trait == "Ni"|trait == "As"|trait == "Cd"),
+            leg.name = "Enrichment Factor",grad.value = c(0,0.67,1,1.5,2,3,4), 
+            grad.tag = c(0,0.67,1,1.5,2,3,4), dir = "riskAssment/krig/",file = "krig_enrichmentfactor.png",
+            lonRange = c(119.2,121.8),latRange = c(33.7,35))  -> plot.ef
+
+ggsave(filename = "riskAssment/krig/gather_krig_risk.png",  
+       plot = grid.arrange(plot.igeo,plot.ef, ncol = 1, widths = c(15), heights = c(10,10)))
