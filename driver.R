a5d7c4a4eeaa6540b308851df3892a456839c64e
honghl <honghlaries@126.com>
2017-06-27 23:35:01 +0800

    driver of element var

---
+++ driver.R
@@ -0,0 +1,34 @@
+## Initialization
+rm(list = ls())
+source("constant.R");
+pkgInitialization(c("dplyr","tidyr","ggplot2"))
+
+## Functions 
+relationPlot <- function(dat, fact, resp, col) {
+  ggplot() +
+    geom_point(aes_string(x = fact, y = resp), col = "black", data = dat) +
+    geom_smooth(aes_string(x = fact, y = resp), method = "loess", col = col, fill = col, data = dat) +
+    theme_bw()
+}
+
+relationPlot.gather <- function(dat,tag) {
+  plot.dist <- relationPlot(dat, "distance", tag, "green")
+  plot.clay <- relationPlot(dat, "clay", tag, "black")
+  plot.dep <- relationPlot(dat, "depth", tag, "blue")
+  plot.al <- relationPlot(dat, "Al", tag, "purple")
+  plot.h <- relationPlot(dat, "proton", tag, "orange")
+  plot.sal <- relationPlot(dat, "salinity", tag, "brown")
+  grid.arrange(plot.dist,plot.dep,plot.clay,plot.al,plot.h,plot.sal, nrow=2, ncol=3)
+}
+
+## Example
+dat <- datareadln() %>%
+  dplyr::select(depth,distance,salinity:sand) %>% 
+  dplyr::mutate(proton = 10^(-pH))
+
+taglist <- c("Cr","As","Ni","Cu","Pb","Zn","Cd")
+
+for(i in 1:length(taglist)) {
+  relationPlot.gather(dat,taglist[i])
+  ggsave(paste(dirPreset("relation/driver"),"/",taglist[i],".png",sep = ""),dpi = 600)
+}
