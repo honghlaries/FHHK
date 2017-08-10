462de8c8d1b4458c9752411526303a04bf5aaf76
honghlaries@126.com <honghlaries@126.com>
2017-08-11 05:02:31 +0800

    factor : latex convey formating

--- convey.R
+++ convey.R
@@ -4,11 +4,23 @@ source("uniTls_pkgInstall.R");source("uniTls_presetPaths.R");source("uniTls_csv2
 pkgInitialization(c("dplyr","tidyr"))
 
 ## Functions 
+sigNotation <- function(x) {
+  if (x < 0.001) "***" else if (x < 0.01) "**" else if (x < 0.05) "*" else x
+}
 
 ## Example
 taglist <- c("Cr","As","Ni","Cu","Pb","Zn","Cd")
 
 for(i in 1:length(taglist)) {
   dat <- read.csv(paste("relation/driver/polyRegression_",taglist[i],".csv",sep = ""))
+  dat <- dat[,-6:-8]
+  dat <- dplyr::transmute_(dat, "parameter" = "parameter",
+                           "Estimate" = "signif(Estimate,3)",
+                           "Std..Error" = "signif(Std..Error,3)",
+                           "t.value" = "signif(t.value,3)",
+                           "p.Estimate." = "signif(p.Estimate.,2)",
+                           "F.value" = "signif(F.value,3)",
+                           "p.ANOVA." = "signif(p.ANOVA.,2)")
+  colnames(dat)[c(5,7)] <- c("p(Estimate)","p(ANOVA)")
   conveyLaTex(dat,paste("relation/driver/polyRegression_",taglist[i],"_latexcode.txt",sep = ""))
 }
