2abb35ec3c0c0b3893a709875c53d5ae0a3adfd2
honghlaries@126.com <honghlaries@126.com>
2017-08-11 04:45:02 +0800

    filename

--- uniTls_csv2latex.R
+++ uniTls_csv2latex.R
@@ -1,14 +1,14 @@
 conveyLaTex <- function(dat,filename) {
   dat <- as.data.frame(dat)
-  cat("\\toprule \n", file = "relation/test.txt",append = F)
-  cat(colnames(dat), file = "relation/test.txt", sep = " & ",append = T)
-  cat(" \\\\ \n", file = "relation/test.txt",append = T)
-  cat("\\midrule \n", file = "relation/test.txt",append = T)
+  cat("\\toprule \n", file = filename,append = F)
+  cat(colnames(dat), file = filename, sep = " & ",append = T)
+  cat(" \\\\ \n", file = filename,append = T)
+  cat("\\midrule \n", file = filename,append = T)
   for(i in 1:length(rownames(dat))) {
-    cat(unlist(dat[i,]), file = "relation/test.txt", sep = " & ",append = T)
-    cat(" \\\\ \n", file = "relation/test.txt",append = T)
+    cat(unlist(dat[i,]), file = filename, sep = " & ",append = T)
+    cat(" \\\\ \n", file = filename,append = T)
   }
-  cat("\\bottomrule \n", file = "relation/test.txt",append = T)
+  cat("\\bottomrule \n", file = filename,append = T)
 }
 
 
