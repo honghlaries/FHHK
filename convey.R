82a3ad3f7c4292451597bdd337bdf2d0b5bfca00
honghlaries@126.com <honghlaries@126.com>
2017-08-12 00:44:18 +0800

    convey refined model to LaTeX

--- convey.R
+++ convey.R
@@ -13,7 +13,7 @@ sigNotation <- function(x) {
 taglist <- c("Cr","As","Ni","Cu","Pb","Zn","Cd")
 
 for(i in 1:length(taglist)) {
-  dat <- read.csv(paste("relation/driver/polyRegression_",taglist[i],".csv",sep = ""))
+  dat <- read.csv(paste("relation/driver/multiRegExplo_",taglist[i],".csv",sep = ""))
   dat <- dat[,-6:-8]
   dat <- dplyr::transmute_(dat, "parameter" = "parameter",
                            "Estimate" = "signif(Estimate,3)",
@@ -26,5 +26,22 @@ for(i in 1:length(taglist)) {
   for(j in 1:length(dat$p.Estimate.)) dat$p.ANOVA.[j] <- sigNotation(as.numeric(dat$p.ANOVA.[j]) )
   colnames(dat)[c(5,7)] <- c("p(Estimate)","p(ANOVA)")
   dat$parameter <- as.character(dat$parameter)
-  conveyLaTex(dat,paste("relation/driver/polyRegression_",taglist[i],"_latexcode.txt",sep = ""))
+  conveyLaTex(dat,paste("relation/driver/multiRegExplo_",taglist[i],"_latexcode.txt",sep = ""))
+}
+
+for(i in 1:length(taglist)) {
+  dat <- read.csv(paste("relation/driver/multiRegRefined_",taglist[i],".csv",sep = ""))
+  dat <- dat[,-6:-8]
+  dat <- dplyr::transmute_(dat, "parameter" = "parameter",
+                           "Estimate" = "signif(Estimate,3)",
+                           "Std..Error" = "signif(Std..Error,3)",
+                           "t.value" = "signif(t.value,3)",
+                           "p.Estimate." = "signif(p.Estimate.,2)",
+                           "F.value" = "signif(F.value,3)",
+                           "p.ANOVA." = "signif(p.ANOVA.,2)")
+  for(j in 1:length(dat$p.Estimate.)) dat$p.Estimate.[j] <-sigNotation(as.numeric(dat$p.Estimate.[j]) )
+  for(j in 1:length(dat$p.Estimate.)) dat$p.ANOVA.[j] <- sigNotation(as.numeric(dat$p.ANOVA.[j]) )
+  colnames(dat)[c(5,7)] <- c("p(Estimate)","p(ANOVA)")
+  dat$parameter <- as.character(dat$parameter)
+  conveyLaTex(dat,paste("relation/driver/multiRegRefined_",taglist[i],"_latexcode.txt",sep = ""))
 }
