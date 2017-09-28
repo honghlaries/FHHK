9ecb8bf19ce64ea1c869f75ffe803ccc099c85ca
honghl <honghlaries@126.com>
2017-09-29 06:59:50 +0800

    regroup elem contents map

--- elemKrig.R
+++ elemKrig.R
@@ -9,20 +9,17 @@ source("grid.R")
 spView.elem <- function(elem,...) {
   spView(dat = grid.value.tot %>% 
            filter(trait == elem),
-         leg.name = paste(elem,"(mg/kg)",sep = " "),
+         leg.name = elem,
          lonRange = lonRange, latRange = latRange) +
     geom_polygon(aes(x = long, y = lat, group = group), 
                  colour = "black", fill = "grey80", data = fortify(readShapePoly("data/bou2_4p.shp"))) +
-    geom_point(aes(x = lon, y = lat), color = "black", data = sites) +
-    theme(#legend.key.height = unit(5, "mm"),
-          axis.text = element_blank(),
+    theme(axis.text = element_blank(),
           axis.ticks = element_blank())
 }
 
 ## Example
 dat <- datareadln() %>%
-  dplyr::mutate(AvsRatio = AVS / orgC) %>%
-  tidyr::gather(trait,value,depth,Al:AvsRatio) %>%
+  tidyr::gather(trait,value,depth,Pb:Cd) %>%
   dplyr::group_by(siteID,trait,lon,lat) %>%
   dplyr::summarise(value = mean(value)) %>%
   tidyr::spread(trait,value)
@@ -31,93 +28,99 @@ dat <- as.data.frame(dat)
 coordinates(dat) <- ~lon+lat
 
 grid.value.tot <- NULL
-grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Al)~1, tag = "Al", cutoff = 1.2, 
-                                   modsel = vgm(0.15,"Sph",0.5), dir = "element/krig")) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Al")))
+#grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Al)~1, tag = "Al", cutoff = 1.2, 
+#                                   modsel = vgm(0.15,"Sph",0.5), quietmode = T)) %>% 
+#  select(lon, lat, value = var1.pred)
+#grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Al")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Fe) ~ 1, tag = "Fe", cutoff = 1.5,
-                                   modsel = vgm(0.01,"Mat",0.5,0.01,kappa = 1), dir = "element/krig")) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Fe")))
-
-grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Mn) ~ 1, tag = "Mn", cutoff = 1.5,
-                                   modsel = vgm(0.035,"Sph",0.3), dir = "element/krig")) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Mn")))
+#grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Fe) ~ 1, tag = "Fe", cutoff = 1.5,
+#                                   modsel = vgm(0.01,"Mat",0.5,0.01,kappa = 1), quietmode = T)) %>% 
+#  select(lon, lat, value = var1.pred)
+#grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Fe")))
+#
+#grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Mn) ~ 1, tag = "Mn", cutoff = 1.5,
+#                                   modsel = vgm(0.035,"Sph",0.3), quietmode = T)) %>% 
+#  select(lon, lat, value = var1.pred)
+#grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Mn")))
 
 grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Pb) ~ 1, tag = "Pb", cutoff = 1.5,
-                                   modsel = vgm(0.35,"Sph",0.5), dir = "element/krig")) %>% 
+                                   modsel = vgm(0.35,"Sph",0.5), quietmode = T)) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Pb")))
 
 grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Cr) ~ 1, tag = "Cr", cutoff = 1.5,
-                                   modsel = vgm(0.06,"Mat",0.7,kappa = 1), dir = "element/krig")) %>% 
+                                   modsel = vgm(0.06,"Mat",0.7,kappa = 1), quietmode = T)) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Cr")))
 
 grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Ni) ~ 1, tag = "Ni", cutoff = 1.5,
-                                   modsel = vgm(0.06,"Sph",1), dir = "element/krig")) %>% 
+                                   modsel = vgm(0.06,"Sph",1), quietmode = T)) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Ni")))
 
 grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Cu) ~ 1, tag = "Cu", cutoff = 1.5,
-                                   modsel = vgm(0.25,"Sph",0.75), dir = "element/krig")) %>% 
+                                   modsel = vgm(0.25,"Sph",0.75), quietmode = T)) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Cu")))
 
 grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Zn) ~ 1, tag = "Zn", cutoff = 1.5,
-                                   modsel = vgm(0.1,"Sph",0.5), dir = "element/krig")) %>% 
+                                   modsel = vgm(0.1,"Sph",0.5), quietmode = T)) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Zn")))
 
 grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(As) ~ 1, tag = "As", cutoff = 1.5,
-                                   modsel = vgm(0.3,"Sph",0.75), dir = "element/krig")) %>% 
+                                   modsel = vgm(0.3,"Sph",0.75), quietmode = T)) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "As")))
 
 grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(Cd) ~ 1, tag = "Cd", cutoff = 1.5,
-                                   modsel = vgm(0.2,"Sph",0.7), dir = "element/krig")) %>% 
+                                   modsel = vgm(0.2,"Sph",0.7), quietmode = T)) %>% 
   select(lon, lat, value = var1.pred)
 grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "Cd")))
 
-grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(C) ~ 1, tag = "C", cutoff = 1.5,
-                                   modsel = vgm(0.15,"Sph",0.7), dir = "element/krig")) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "C")))
-
-grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(orgC) ~ 1, tag = "orgC", cutoff = 1.5,
-                                   modsel = vgm(0.06,"Mat",0.7,kappa = 1), dir = "element/krig")) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "orgC")))
-
-grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(N) ~ 1, tag = "N", cutoff = 1.5,
-                                   modsel = vgm(0.25,"Sph",1), dir = "element/krig")) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "N")))
-
-grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(S) ~ 1, tag = "S", cutoff = 1.5,
-                                   modsel = vgm(0.4,"Sph",1), dir = "element/krig")) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "S")))
-
-grid.value <- as.data.frame(doKrig(dat, dat.grid, krigFormula = log(S) ~ 1, tag = "AVS", cutoff = 1.5,
-                                   modsel = vgm(0.4,"Sph",1), dir = "element/krig")) %>% 
-  select(lon, lat, value = var1.pred)
-grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, trait = "AVS")))
 
 grid.value.tot$value <- exp(grid.value.tot$value)
 
 
-plot.cu <- spView.elem("Cu");ggsave(filename = "element/krig/Cu.png")
-plot.zn <- spView.elem("Zn");ggsave(filename = "element/krig/Zn.png")
-plot.pb <- spView.elem("Pb");ggsave(filename = "element/krig/Pb.png")
-plot.cr <- spView.elem("Cr");ggsave(filename = "element/krig/Cr.png")
-plot.ni <- spView.elem("Ni");ggsave(filename = "element/krig/Ni.png")
-plot.as <- spView.elem("As");ggsave(filename = "element/krig/As.png")
-plot.cd <- spView.elem("Cd");ggsave(filename = "element/krig/Cd.png")
+guides.elem <- guides(fill = guide_colourbar(barwidth = 1, barheight = 6))
 
-p <- grid.arrange(plot.cu,plot.zn,plot.pb,plot.cr,plot.ni,plot.as,plot.cd,
-             nrow = 4, ncol = 2, widths = c(11,11), heights = rep(7,4))
+plot.cu <- spView.elem("Cu") + guides.elem +
+  geom_point(aes(x = lon, y = lat), color = "white", size = 2,
+             data = as.data.frame(dat[dat$Cu >= quantile(read.csv("data/result_element.csv")$Cu, 
+                                                         probs = 0.75),]))
 
-ggsave(filename = "element/krig/gather_krig_element.png", plot = p)
+plot.zn <- spView.elem("Zn") + guides.elem +
+  geom_point(aes(x = lon, y = lat), color = "white", size = 2,
+             data = as.data.frame(dat[dat$Zn >= quantile(read.csv("data/result_element.csv")$Zn, 
+                                                         probs = 0.75),]))
+
+plot.pb <- spView.elem("Pb") + guides.elem +
+  geom_point(aes(x = lon, y = lat), color = "white", size = 2,
+             data = as.data.frame(dat[dat$Pb >= quantile(read.csv("data/result_element.csv")$Pb, 
+                                                         probs = 0.75),]))
+  
+plot.cr <- spView.elem("Cr") + guides.elem +
+  geom_point(aes(x = lon, y = lat), color = "white", size = 2,
+             data = as.data.frame(dat[dat$Cr >= quantile(read.csv("data/result_element.csv")$Cr, 
+                                                         probs = 0.75),]))
+
+plot.ni <- spView.elem("Ni") + guides.elem +
+  geom_point(aes(x = lon, y = lat), color = "white", size = 2,
+             data = as.data.frame(dat[dat$Ni >= quantile(read.csv("data/result_element.csv")$Ni, 
+                                                         probs = 0.75),]))
+
+plot.as <- spView.elem("As") + guides.elem +
+  geom_point(aes(x = lon, y = lat), color = "white", size = 2,
+             data = as.data.frame(dat[dat$As >= quantile(read.csv("data/result_element.csv")$As, 
+                                                         probs = 0.75),]))
+
+plot.cd <- spView.elem("Cd") + guides.elem +
+  geom_point(aes(x = lon, y = lat), color = "white", size = 2,
+             data = as.data.frame(dat[dat$Cd >= quantile(read.csv("data/result_element.csv")$Cd, 
+                                                         probs = 0.75),]))
+
+p <- grid.arrange(plot.pb,plot.ni,plot.cu,plot.zn,plot.cr,plot.as,plot.cd,
+             ncol = 2, widths = c(11,11))
+
+ggsave(filename = "element/krig/gather_krig_element.png", plot = p, 
+       dpi = 600, width = 8, height = 8)
