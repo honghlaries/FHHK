2b4decb89c931446bcd5b9eab5d700eb39305f8a
honghl <honghlaries@126.com>
2017-09-30 10:11:31 +0800

    cluster: sample level

--- cluster.R
+++ cluster.R
@@ -4,7 +4,6 @@ source("constant.R");source("anaTls_multivariate.R");source("anaTls_spatialView.
 pkgInitialization(c("dplyr","tidyr","sp","gdata","gridExtra","ggplot2","maptools"))
 dirInitialization("hca")
 bkmap <- readShapePoly("data/bou2_4p.shp")
-lonRange = c(119.2,121.8);latRange = c(33.7,35)
 source("grid.R")
 
 # Functions 
@@ -111,7 +110,7 @@ for(i in 1:length(tmp)) {
 }
 tmp <- c("Pb","Cr","Ni","Cu","Zn","As","Cd")
 for(i in 1:length(tmp)) {
-  cache[dat$trait == tmp[i]] <- "Traget Heavy Metal" 
+  cache[dat$trait == tmp[i]] <- "Trageted Heavy Metal" 
 }
 dat <- data.frame(dat, taggroup = cache)
 
@@ -138,57 +137,136 @@ cluster.samp <- hcluster(dat %>% select(Pb:Cd),
 
 plot.ca.tree <- plot(cluster.samp) 
 
-dat1 <- data.frame(dat,class = cutree(cluster.samp,5)) %>%
+dat1 <- data.frame(dat,class = cutree(cluster.samp,4)) %>%
   group_by(siteID) %>%
   summarise(group1 = sum(class == 1)/n(),group2 = sum(class == 2)/n(),
-            group3 = sum(class == 3)/n(),group4 = sum(class == 4)/n(),
-            group5 = sum(class == 5)/n()) %>%
+            group3 = sum(class == 3)/n(),group4 = sum(class == 4)/n()) %>%
   dplyr::inner_join(read.csv("data/meta_sites.csv"), by = c("siteID" = "siteID")) %>%
-  select(lon,lat,group1:group5)
+  select(lon,lat,siteID,group1:group4)
   
 dat1 <- as.data.frame(dat1)
 coordinates(dat1) <- ~lon+lat
 
-plot.ca.sp <- ggplot() + 
-  geom_point(aes(x = lon, y = lat, col = class), 
-             size = 2, data = as.data.frame(dat1)) +
+grid.value.tot <- NULL
+grid.value <- as.data.frame(doKrig(dat1, dat.grid, tag = "group1", cutoff = 1.7,
+                                   modsel = vgm(0.15,"Sph",1), quietmode = T)) %>%  
+  select(lon, lat, value = var1.pred) 
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, class = 1)))
+
+grid.value <- as.data.frame(doKrig(dat1, dat.grid, tag = "group2", cutoff = 2,
+                                   modsel = vgm(0.25,"Sph",1,0), quietmode = T)) %>%  
+  select(lon, lat, value = var1.pred) 
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, class = 2)))
+
+grid.value <- as.data.frame(doKrig(dat1, dat.grid, tag = "group3", cutoff = 1,
+                                   modsel = vgm(0.15,"Lin",0,0.15), quietmode = T)) %>%  
+  select(lon, lat, value = var1.pred) 
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, class = 3)))
+
+grid.value <- as.data.frame(doKrig(dat1, dat.grid, tag = "group4", cutoff = 1.6,
+                                   modsel = vgm(0.10,"Sph",0.5), quietmode = T)) %>%  
+  select(lon, lat, value = var1.pred) 
+grid.value.tot <- rbind(grid.value.tot, as.data.frame(cbind(grid.value, class = 4)))
+
+nym <- function(a,b) {
+  if(abs(a - b) <= 0.00001) a else 0
+}
+
+grid.value.tot1 <- grid.value.tot %>%
+  dplyr::mutate(class = paste("c",class,sep = "")) %>%
+  tidyr::spread(class,value) 
+for (i in 1: length(grid.value.tot1$c1)) {
+  tag <- with(grid.value.tot1[i,], max(c1,c2,c3,c4))
+  grid.value.tot1[i,"c1"] <- nym(grid.value.tot1[i,"c1"], tag)
+  grid.value.tot1[i,"c2"] <- nym(grid.value.tot1[i,"c2"], tag)
+  grid.value.tot1[i,"c3"] <- nym(grid.value.tot1[i,"c3"], tag)
+  grid.value.tot1[i,"c4"] <- nym(grid.value.tot1[i,"c4"], tag)
+}
+grid.value.tot1 <- grid.value.tot1 %>%
+  tidyr::gather(class,value,c1:c4) %>%
+  dplyr::filter(value != 0) 
+  
+plot.ca.indicator1 <- ggplot() + 
+  geom_raster(aes(x = lon, y = lat, fill = class),
+              interpolate = T, show.legend = F, data = grid.value.tot1) +
   geom_polygon(aes(x = long, y = lat, group = group), 
                colour = "black", fill = "grey80", data = fortify(bkmap)) +
+  geom_text(aes(x = c(),
+                y = c(),
+                label = c(),
+                angle = c())) + 
+  scale_fill_grey() +
+  xlab("") + ylab("") +
   coord_quickmap(xlim = lonRange, ylim = latRange) +
   theme_bw() + 
-  theme(aspect.ratio = 1/2,
+  theme(aspect.ratio = (latiRange[2]-latiRange[1])/(longiRange[2]-longiRange[1]),
         panel.grid = element_blank(),
         strip.background = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
         legend.position = "none")
 
-doKrig(dat1, dat.grid, tag = "group1", cutoff = 2, 
-       modsel = vgm(0.1,"Mat",1,0.02,kappa = 1), 
-       dir = "hca/samp/krig/group1") -> tmp
-grid.value <- as.data.frame(tmp) %>%  
-  select(lon, lat, value = var1.pred) %>%
-  mutate(value = (value>=0.5)) %>%
-  as.data.frame()
+grid.value.tot2 <- grid.value.tot %>%
+  dplyr::filter(value >= 0.5)  
 
-plot.ca.group1 <- ggplot() + 
-  geom_raster(aes(x = lon, y = lat, fill = value),
-              interpolate = T, show.legend = T, data = grid.value) +
-  geom_point(aes(x = lon, y = lat), 
-             size = 2, data = as.data.frame(dat1)) +
+plot.ca.indicator2 <- ggplot() + 
+  geom_raster(aes(x = lon, y = lat, fill = class),
+              interpolate = T, show.legend = F, data = grid.value.tot2) +
   geom_polygon(aes(x = long, y = lat, group = group), 
                colour = "black", fill = "grey80", data = fortify(bkmap)) +
-  scale_color_grey(start = 0.8, end = 0.2) +
-  scale_alpha_continuous(range = c(0,0.8)) +
+  #scale_fill_grey() +
+  xlab("") + ylab("") +
   coord_quickmap(xlim = lonRange, ylim = latRange) +
   theme_bw() + 
-  theme(aspect.ratio = 1/2,
+  theme(aspect.ratio = (latiRange[2]-latiRange[1])/(longiRange[2]-longiRange[1]),
         panel.grid = element_blank(),
         strip.background = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
         legend.position = "none")
 
-## saving plot
-ggsave(plot = plot.ca.sp, filename = "hca/casp.png", dpi = 600)
-ggsave(plot = plot.ca.bar, filename = "hca/cabar.png", dpi = 600)
+dat1 <-data.frame(dat,class = cutree(cluster.samp,4)) %>%  
+  gather(trait, value, Al:depth) %>%
+  group_by(trait,class) %>%
+  summarise(mean = mean(value,na.rm = T),
+            se = sd(value,na.rm= T)/sqrt(n()-sum(is.na(value)))) %>%
+  group_by()%>%
+  mutate(trait = factor(trait,levels = c("sand","silt","clay","depth","Al","Fe",
+                                         "Mn","orgC","AVS","Pb","Cr","Ni","Cu",
+                                         "Zn","As","Cd")),
+         class = factor(class))%>%
+  arrange(trait)
 
-grid.arrange(plot.ca.sp, plot.ca.bar, ncol = 2, widths = c(10,6), heights = 3) -> plot.gather
-ggsave(plot = plot.gather, filename = "hca/gather_hcaPlot.png", dpi = 600)
+cache <- rep(NA,length(row.names(dat1)))
+tmp <- c("sand","silt","clay","depth","Al","Fe","Mn","orgC","AVS")
+for(i in 1:length(tmp)) {
+  cache[dat1$trait == tmp[i]] <- "Environment Factor" 
+}
+tmp <- c("Pb","Cr","Ni","Cu","Zn","As","Cd")
+for(i in 1:length(tmp)) {
+  cache[dat1$trait == tmp[i]] <- "Trageted Heavy Metal" 
+}
+dat1 <- data.frame(dat1, taggroup = cache)
 
+plot.ca.bar <- ggplot(data = dat1) + 
+  geom_bar(aes(x = trait, y = mean, group = class, fill = class), stat = "identity",
+           position = position_dodge(width = 0.9)) +
+  geom_errorbar(aes(x = trait, ymin = mean - se, ymax = mean + se, group = class),
+                width = 0.5 ,size = 0.7, col = "black",
+                position = position_dodge(width = 0.9)) + 
+  facet_wrap(~trait, scale = "free") + 
+  scale_fill_grey() +
+  xlab("") + ylab("") +
+  theme_bw() + 
+  theme(aspect.ratio = 1,
+        panel.grid = element_blank(),
+        strip.background = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "right") 
+
+plot.gather <- grid.arrange(plot.ca.indicator1,plot.ca.bar,
+               ncol = 2, widths = c(10,6))
+
+ggsave(filename = "hca/gather_hcaPlot.png", plot = plot.gather, 
+       dpi = 600, width = 16, height = 6)
