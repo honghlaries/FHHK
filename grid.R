4b1b6ff89666f4fda76f446ff1e9879a10e641b5
honghl <honghlaries@126.com>
2017-12-17 00:34:57 +0800

    grid rescale

--- grid.R
+++ grid.R
@@ -1,11 +1,18 @@
+landliplon <- 34.2762; landliplat <- 120.2609
+land1lon <- 34.49081; land1lat <- 119.7353
+land2lon <- 32.77474; land2lat <- 120.9608
+
 longiRange <-  seq(from = 119.9, to = 121.8, length.out = 125)
 latiRange <-  seq(from = 33.7, to = 34.9, length.out = 125)
 dat.grid <- data.frame(lat = c(1), lon = c(1))
 for (i in 1:125) {
   for (j in 1:125) {
-    #if((longiRange[j] - 119.9) * (latiRange[i] - 33.7) - (longiRange[j] - 120.6) * (latiRange[i] - 34.5) > 0) {
+    if(((longiRange[j] - landliplon) * (latiRange[i] - land1lat) - 
+        (longiRange[j] - land1lon) * (latiRange[i] - landliplat)) > 0 ||
+       ((longiRange[j] - landliplon) * (latiRange[i] - land2lat) - 
+        (longiRange[j] - land2lon) * (latiRange[i] - landliplat)) > 0 ){
     dat.grid <- rbind(dat.grid, c(latiRange[i],longiRange[j]))
-    #}
+    }
   }
 }
 dat.grid <- dat.grid[-1,]
--- grid_resamp.R
+++ grid_resamp.R
@@ -1,11 +1,18 @@
+landliplon <- 34.2762; landliplat <- 120.2609
+land1lon <- 34.49081; land1lat <- 119.7353
+land2lon <- 32.77474; land2lat <- 120.9608
+
 longiRange <-  seq(from = 119.9, to = 121.8, length.out = 25)
 latiRange <-  seq(from = 33.7, to = 34.9, length.out = 25)
 dat.grid.resample <- data.frame(lat = c(1), lon = c(1))
 for (i in 1:25) {
   for (j in 1:25) {
-    #if((longiRange[j] - 119.9) * (latiRange[i] - 33.7) - (longiRange[j] - 120.6) * (latiRange[i] - 34.5) > 0) {
-    dat.grid.resample <- rbind(dat.grid.resample, c(latiRange[i],longiRange[j]))
-    #}
+    if(((longiRange[j] - landliplon) * (latiRange[i] - land1lat) - 
+        (longiRange[j] - land1lon) * (latiRange[i] - landliplat)) > 0 ||
+       ((longiRange[j] - landliplon) * (latiRange[i] - land2lat) - 
+        (longiRange[j] - land2lon) * (latiRange[i] - landliplat)) > 0 ){
+      dat.grid <- rbind(dat.grid, c(latiRange[i],longiRange[j]))
+    }
   }
 }
 dat.grid.resample <- dat.grid.resample[-1,]
