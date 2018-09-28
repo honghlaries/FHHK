bae323dfceac1419fedc27a74c4c9a05ef994429
Hualong Hong <honghlaries@126.com>
2018-09-28 14:44:28 +0800

    elem rose: Add direction

--- elemKrig_rose.R
+++ elemKrig_rose.R
@@ -1,8 +1,7 @@
 ## Initialization
 rm(list = ls())
-source("constant.R");source("anaTls_spatialView.R");source("uniTls_csv2latex.R")
-pkgInitialization(c("dplyr","tidyr","sp","gstat","ggplot2","RColorBrewer","gridExtra"))
-source("grid.R");source("grid_resamp.R")
+source("constant.R");source("anaTls_spatialView.R")
+pkgInitialization(c("dplyr","tidyr","gstat","ggplot2","RColorBrewer","gridExtra"))
 dirInitialization(c("element","element/krig"))
 
 ## Functions 
@@ -42,113 +41,83 @@ azimuthCalc.rough <- azimuthCalc <- function(lata, lona, latb, lonb){
 
 
 ## data calculation
-grid.perm.tot <- read.csv("data/result_element_perm.csv") 
 
-grid.content.tot <- NULL
+### sample dataset
 
-seldat <- grid.perm.tot %>%
-  dplyr::filter(trait == "Pb") %>%
-  dplyr::mutate(delta = var.samp-var.site)
-coordinates(seldat) <- ~lon+lat
-grid.content.mean <- as.data.frame(doKrig(seldat, dat.grid, tag = "mean",
-                                          cutoff = 1,
-                                          modsel = vgm(0.02,"Sph",0.5),
-                                          quietmode = T)) %>%
-  dplyr::select(lon, lat, mean = var1.pred)
-grid.content.tot <- rbind(grid.content.tot,
-                          as.data.frame(cbind(grid.content.mean,
-                                              trait = "Pb")))
+grid.samp.tot <- datareadln() %>%
+  dplyr::select(lon, lat, Pb:Cd) %>%
+  tidyr::gather(trait, value, Pb:Cd)
+grid.samp.tot <- grid.samp.tot %>% dplyr::select(lon, lat, value, trait)
 
-seldat <- grid.perm.tot %>% filter(trait == "Cr") 
-coordinates(seldat) <- ~lon+lat
-grid.content.mean <- as.data.frame(doKrig(seldat, dat.grid, tag = "mean", 
-                                          cutoff = 1.5, 
-                                          modsel = vgm(0.04,"Sph",0.5), 
-                                          quietmode = T)) %>% 
-  dplyr::select(lon, lat, mean = var1.pred)
-grid.content.tot <- rbind(grid.content.tot, 
-                          as.data.frame(cbind(grid.content.mean, 
-                                              trait = "Cr")))
+grid.samp.tot$dist <- distCalc(coo.oldmouth$lat, coo.oldmouth$lon, 
+                              grid.samp.tot$lat, grid.samp.tot$lon)
 
-seldat <- grid.perm.tot %>% filter(trait == "Ni") 
-coordinates(seldat) <- ~lon+lat
-grid.content.mean <- as.data.frame(doKrig(seldat, dat.grid, tag = "mean", 
-                                          cutoff = 1.5, 
-                                          modsel = vgm(0.05, "Mat", 1, kappa = 2), 
-                                          quietmode = T)) %>% 
-  dplyr::select(lon, lat, mean = var1.pred)
-grid.content.tot <- rbind(grid.content.tot, 
-                          as.data.frame(cbind(grid.content.mean,
-                                              trait = "Ni")))
-
-seldat <- grid.perm.tot %>% filter(trait == "Cu")
-coordinates(seldat) <- ~lon+lat
-grid.content.mean <- as.data.frame(doKrig(seldat, dat.grid, tag = "mean", 
-                                          cutoff = 1, 
-                                          modsel = vgm(0.15, "Mat", 0.6, kappa = 3), 
-                                          quietmode = T)) %>% 
-  dplyr::select(lon, lat, mean = var1.pred)
-grid.content.tot <- rbind(grid.content.tot, 
-                          as.data.frame(cbind(grid.content.mean,
-                                              trait = "Cu")))
-
-seldat <- grid.perm.tot %>% filter(trait == "Zn") 
-coordinates(seldat) <- ~lon+lat
-grid.content.mean <- as.data.frame(doKrig(seldat, dat.grid, tag = "mean", 
-                                          cutoff = 0.7, 
-                                          modsel = vgm(0.15, "Mat", 0.6, kappa = 3), 
-                                          quietmode = T)) %>% 
-  dplyr::select(lon, lat, mean = var1.pred)
-grid.content.tot <- rbind(grid.content.tot, 
-                          as.data.frame(cbind(grid.content.mean,
-                                              trait = "Zn")))
-
-seldat <- grid.perm.tot %>% filter(trait == "Cd") 
-coordinates(seldat) <- ~lon+lat
-grid.content.mean <- as.data.frame(doKrig(seldat, dat.grid, tag = "mean",
-                                          cutoff = 0.8,
-                                          modsel = vgm(0.15, "Mat", 0.6, kappa = 3.5),
-                                          quietmode = T)) %>%
-  dplyr::select(lon, lat, mean = var1.pred)
-grid.content.tot <- rbind(grid.content.tot, 
-                          as.data.frame(cbind(grid.content.mean,
-                                              trait = "Cd")))
-
-grid.content.tot$value <- exp(grid.content.tot$mean) 
-grid.content.tot <- grid.content.tot %>% dplyr::select(-mean)
-remove(grid.content.mean, grid.perm.tot, seldat)
-
-grid.content.tot$dist <- distCalc(coo.oldmouth$lat, coo.oldmouth$lon, 
-                                  grid.content.tot$lat, grid.content.tot$lon)
-
-for(i in 1:length(grid.content.tot$lon)) {
-  if (!i%%1000) print(paste("calculating:",i,"/",length(grid.content.tot$lon)))
-  grid.content.tot$azi[i] <- azimuthCalc.rough(coo.oldmouth$lat, coo.oldmouth$lon, 
-                                      grid.content.tot$lat[i], grid.content.tot$lon[i])
+for(i in 1:length(grid.samp.tot$lon)) {
+  if (!i%%1000) print(paste("calculating:",i,"/",length(grid.samp.tot$lon)))
+  grid.samp.tot$azi[i] <- azimuthCalc.rough(coo.oldmouth$lat, coo.oldmouth$lon, 
+                                            grid.samp.tot$lat[i], grid.samp.tot$lon[i])
 }
 
-grid.content.tot$azideg <- ceiling(grid.content.tot$azi/3/pi*180) *3
+grid.samp.tot$azideg <- floor(grid.samp.tot$azi/pi*180) 
 
-grid.content.tot$azideggroup <- ceiling((grid.content.tot$azideg)/30) 
-#grid.content.tot$azideggroup[grid.content.tot$azideg > 337.5] <- 9
+grid.samp.tot$azideggroup <- ceiling((grid.samp.tot$azideg)/30) 
+#grid.samp.tot$azideggroup[grid.samp.tot$azideg > 337.5] <- 1
+grid.samp.tot$azideggroup <- factor(grid.samp.tot$azideggroup, 
+                                    levels = 1:12,
+                                    labels = c("NEE","NE","NEN",
+                                               "NWN","NW","NWW",
+                                               "SWW","SW","SWE",
+                                               "SES","SE","SEE"))
+
+### perm dataset
+grid.perm.tot <- read.csv("data/result_element_perm.csv") %>%
+  dplyr::filter(trait != "As")
+
+grid.perm.tot$value <- exp(grid.perm.tot$mean) 
+grid.perm.tot <- grid.perm.tot %>% dplyr::select(lon, lat, value, trait)
+
+grid.perm.tot$dist <- distCalc(coo.oldmouth$lat, coo.oldmouth$lon, 
+                                  grid.perm.tot$lat, grid.perm.tot$lon)
+
+for(i in 1:length(grid.perm.tot$lon)) {
+  if (!i%%1000) print(paste("calculating:",i,"/",length(grid.perm.tot$lon)))
+  grid.perm.tot$azi[i] <- azimuthCalc.rough(coo.oldmouth$lat, coo.oldmouth$lon, 
+                                      grid.perm.tot$lat[i], grid.perm.tot$lon[i])
+}
+
+grid.perm.tot$azideg <- floor(grid.perm.tot$azi/pi*180) 
+
+grid.perm.tot$azideggroup <- ceiling((grid.perm.tot$azideg)/30) 
+#grid.perm.tot$azideggroup[grid.perm.tot$azideg > 337.5] <- 1
+grid.perm.tot$azideggroup <- factor(grid.perm.tot$azideggroup, 
+                                    levels = 1:12,
+                                    labels = c("NEE","NE","NEN",
+                                               "NWN","NW","NWW",
+                                               "SWW","SW","SWE",
+                                               "SES","SE","SEE"))
 
 p <- 
   ggplot() +
-  geom_point(aes(x = azideg, y = value, col = dist), alpha = 0.05, 
-               data = grid.content.tot) +
+  geom_point(aes(x = azideg, y = value, col = dist), alpha = 0.5, 
+               data = grid.perm.tot) +
+  scale_x_continuous(limits = c(0,360), breaks = 45* 0:8, labels = c("E","NE","N","NW","W","SW","S","SE","E")) + 
   scale_color_gradient(low = "yellow", high = "blue") +
   #coord_polar(start = -pi/2, direction = -1) +
   facet_wrap(~trait, scale = "free") +
   theme_bw() +
   theme(axis.title = element_blank(),
-        legend.position = "none")
+        legend.position = "right")
 ggsave(plot = p, filename = "element/gather_pointrose_element.png", width = 8, height = 6, dpi = 300)
 rm(p)
 
 p <- 
   ggplot() +
-  geom_point(aes(x = dist, y = value), alpha = 0.01, data = grid.content.tot) +
-  #geom_smooth(aes(x = dist, y = value), method = "loess", data = grid.content.tot) +
+  geom_point(aes(x = dist, y = value),
+             alpha = 0.25, data = grid.perm.tot) +
+  geom_smooth(aes(x = dist, y = value), 
+              method = "loess", 
+              #method.args = list(family = "possion", link = "logit"),
+              data = grid.perm.tot) +
   #coord_polar(start = -pi/2, direction = -1) +
   facet_grid(trait~azideggroup, scale = "free_y") +
   theme_bw() +
